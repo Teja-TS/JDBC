8d2893d1aa34900d54995f202d5f16e2f5ce5de0
Teja-TS <vtu22216@veltech.edu.in>
2026-02-04 16:57:15 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,12 @@
+package Javalearning;
+
+public class First {
+     public static void main(String[] args) {
+    	 String str1 = "hello";
+    	
+     String str2 = "good morning";
+	 System.out.println(str1.concat(str2));
+
+     
+	}
+}
